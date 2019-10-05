12593cf499bfd4e592090b719abe37555ed673a9
Hafizudin <ziden.storm@gmail.com>
2019-10-05 14:48:12 +0800

    complete

--- app/src/main/java/com/example/asdscreening/SecondQuestionActivity.java
+++ app/src/main/java/com/example/asdscreening/SecondQuestionActivity.java
@@ -1,5 +1,6 @@
 package com.example.asdscreening;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,22 +8,27 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondQuestionActivity extends AppCompatActivity {
+public class SecondQuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView ivq21;
 
     public void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_question);
 
-        ivq21 = findViewById(R.id.)
-
-
-        Intent intent= new Intent(SecondQuestionActivity.this, ThirdQuestionActivity.class);
-        startActivity(intent);
+        ivq21 = findViewById(R.id.iv_q21);
+        ivq21.setOnClickListener(this);
 
     }
 
 
+    @Override
+    public void onClick(View view) {
+        if(view == ivq21) {
+            Intent intent = new Intent(SecondQuestionActivity.this, ThirdQuestionActivity.class);
+            startActivity(intent);
+        }
 
+    }
 }
